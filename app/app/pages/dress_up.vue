e17b590b6e9c2ced06f0283049a9e3a0b9c2702d
Hailey Lan <haileylan404@gmail.com>
2026-05-04 13:55:39 -0400

    uwu

---
+++ app/app/pages/dress_up.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style>
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+body {
+    width: 100%;
+    height: 100vh;
+    background-image: url('/insideBg.png');
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+    z-index: 0;
+}
+</style>
--- app/app/pages/start_page.vue
+++ app/app/pages/start_page.vue
@@ -1,20 +1,20 @@
 <template>
     <h1 class="text-3xl font-bold underline">Magical Dress Up Game</h1>
     
-    <NuxtLink to="/yourpage">
-        <button class="border-b border-2">Click Me</button>
-    </NuxtLink>    
+    <NuxtLink to="/dress_up" class="relative z-50">
+     <button class="relative z-50 border-b border-2">Click Me</button>
+    </NuxtLink>
 
     <div class="fixed inset-0 m-0 p-0">
-        <img src="/logo.png" alt="triple t logo" class="absolute bottom-0 left-0 w-57 h-70">
+        <img src="/logo.png" alt="triple t logo" class="absolute bottom-0 left-0 w-57 h-70 z-10">
     </div>
 </template>
 
-<script setup>
+<script setup lang="ts">
 
 </script>
 
-<style >
+<style>
 * {
     margin: 0;
     padding: 0;
@@ -27,5 +27,6 @@ body {
     background-size: cover;
     background-position: center;
     background-repeat: no-repeat;
+    z-index: 0;
 }
 </style>
