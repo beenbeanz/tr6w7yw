84d3ae1fa6507a56cb0febb806375ca71dfef8cf
Hailey Lan <haileylan404@gmail.com>
2026-05-22 13:33:00 -0400

    dress pieces there

--- app/nuxt.config.ts
+++ app/nuxt.config.ts
@@ -10,5 +10,12 @@ export default defineNuxtConfig({
     plugins: [
       tailwindcss()
     ]
-  }
+  },
+   runtimeConfig: {
+        public: {
+            supabase: {key: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR5b3VxcWp2Z3J6eXN6dnlvdWhpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY5NjE5MzksImV4cCI6MjA5MjUzNzkzOX0.bXqbrI9FtP360_QV3CyTMgWFbCrgRACJYr38mKp1hg8",
+              url: "https://tyouqqjvgrzyszvyouhi.supabase.co"
+            },
+        }
+    }
 })
